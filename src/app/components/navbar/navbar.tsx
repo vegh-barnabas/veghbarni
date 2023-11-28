395592d9c151149c3735e9e83516aae9fb53a143
vegh-barnabas <62122932+vegh-barnabas@users.noreply.github.com>
2023-11-28 23:44:11 +0100

    feature(navbar): add scss and color variables

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Link from 'next/link'
-import styles from './navbar.module.css'
+import styles from './navbar.module.scss'
 import { usePathname } from 'next/navigation'
 
 export default function Navbar() {
